4ce2e5e4ff20bd4d87017d7ddf3f024c155aa581
Rimantė Baltutytė <baltutyte@gmail.com>
2012-10-09 17:39:20 +0200

    Pasitvarkiau kintamuju aprasyma

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@ using namespace std;
 int main()
 {
     cout << "Monte Carlo realizacijos pradzia" << endl;
+    cout << "(RANDOM SEARCH METHOD)" << endl;
     // Kintamuju apsirasymas
     double f, x; // Funkcijos reiksmes ir argumentas x
     int n; // Uzedavinio dimensija
-
+    double xSprendinys; // Argumento
     return 0;
 }
