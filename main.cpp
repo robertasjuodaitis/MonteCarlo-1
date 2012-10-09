7ea64253bf7dd757e926b7b38d2d49dd85e1b839
Rimantė Baltutytė <baltutyte@gmail.com>
2012-10-09 17:33:36 +0200

    Papildziau kintamuju aprasyma

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
     // Kintamuju apsirasymas
     double f, x; // Funkcijos reiksmes ir argumentas x
     int n; // Uzedavinio dimensija
+    int fSprendinys; // Uzdavinio sprendinys
 
     return 0;
 }
