e8d4b596cca391c4ce82a272da55d52082bc1cf1
Remigijus Paulavicius <remziukas@gmail.com>
2012-10-23 17:55:48 +0300

    Istryniau cbp projekto faila, papildziau .gitignore, pataisiau algoritmo isvedima

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-
 using namespace std;
-
 int main()
 {
     cout << "Monte Carlo realizacijos pradzia" << endl;
@@ -17,18 +15,21 @@ int main()
 
     double virsutinis_rezis, apatinis_rezis; // apsirasome intervalo rezius
 
-    cout << "Iveskite inervalo rerzius:" << endl;
+    cout << "Iveskite inervalo apatiny ir virsutiny rezius:" << endl;
     cin >> apatinis_rezis >> virsutinis_rezis;
-    cout << "Iveskite vektoriaus ilgi:" << endl;
+    cout << "Iveskite vektoriaus dimensija (n):" << endl;
     cin >> n;
 
     double vektorius[n];
-
+    cout << "x = ( ";
     for(int i=0; i<n; i++)
     {
         vektorius[i] = rand() * (virsutinis_rezis - apatinis_rezis) / RAND_MAX + apatinis_rezis;
-        cout << i+1  << ". " << vektorius[i] << endl; // Vektoriaus isvedimas i ekrana
     }
-
+    for(int i=0; i<n; i++)
+    {
+        cout << vektorius[i] << " "; // Vektoriaus isvedimas i ekrana
+    }
+    cout << ")";
     return 0;
 }
